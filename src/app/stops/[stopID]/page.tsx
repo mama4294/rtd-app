0ed28af6ab4e095a8b4337c83a02bc82755ba18b
mama4294 <matthew.malone@colorado.edu>
2023-10-26 21:39:45 -0600

    adds background color to light mode trips

--- src/app/stops/[stopID]/page.tsx
+++ src/app/stops/[stopID]/page.tsx
@@ -158,7 +158,7 @@ const Trip = ({ props, index }: { props: Trip; index: number }) => {
   const isSoon = minutesRemaining && minutesRemaining < 60;
 
   return (
-    <div className="group rounded-lg border border-transparent px-5 py-4 transition-colorsborder-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30">
+    <div className="group rounded-lg border border-transparent px-5 py-4 transition-colorsborder-gray-300 bg-gray-300/30 dark:border-neutral-700 dark:bg-neutral-800/30">
       <h2 className={`mb-3 text-xl`}>{ordinal} trip</h2>
 
       <p className={`mb-3 text-2xl font-semibold`}>
