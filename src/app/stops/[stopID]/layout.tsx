14e82d21adddb73a3273c91375778110419acd8b
mama4294 <matthew.malone@colorado.edu>
2023-10-26 21:38:21 -0600

    fixes search params type error

--- src/app/stops/[stopID]/layout.tsx
+++ src/app/stops/[stopID]/layout.tsx
@@ -1,21 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "RTD",
   description: "Get updates on nearby RTD lightrail lines",
 };
 
-export default function Layout({
-  children,
-  params,
-  searchParams,
-}: {
-  children: React.ReactNode;
-  params: { stopID: string };
-  searchParams: { [key: string]: string };
-}) {
-  return <section className={inter.className}>{children}</section>;
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <section>{children}</section>;
 }
