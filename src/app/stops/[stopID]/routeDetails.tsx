b468d6a44e22258b2bb0da2dd9b854fefe454aee
mama4294 <matthew.malone@colorado.edu>
2023-11-27 21:21:11 -0700

    updates routeDetail card header

--- src/app/stops/[stopID]/routeDetails.tsx
+++ src/app/stops/[stopID]/routeDetails.tsx
@@ -7,8 +7,10 @@ const RouteDetails = ({ props }: { props: Branch }) => {
   return (
     <div className="group text-center rounded-lg border border-transparent px-5 py-4 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30">
       <div className="mb-4">
-        <h2 className={`mb-0 text-2xl font-semibold`}>{id} Line</h2>
-        <p className={`mb-2  text-sm opacity-50`}>{directionName}</p>
+        <h2 className={`mb-2 text-2xl font-semibold`}>Upcomming Trains</h2>
+        <p className={`mb-0 text-sm opacity-50`}>
+          {id} Line - {directionName}
+        </p>
         <p className={`mb-0  text-sm opacity-50`}>{routeLongName}</p>
       </div>
       <div className="grid md:grid-cols-3 gap-4 grid-cols-1 ">
