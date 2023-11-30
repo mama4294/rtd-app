f02638b6b78bd783c0431d68194be0b48f97122d
mama4294 <matthew.malone@colorado.edu>
2023-11-30 06:53:13 -0700

    sorts stops in alphebetical order

--- src/constants/Stops.ts
+++ src/constants/Stops.ts
@@ -1,19 +1,304 @@
-const stops = [
+type Stop = {
+  id: number;
+  name: string;
+};
+
+const stops: Stop[] = [
   {
-    name: "I-25 / Broadway Station",
-    id: 33686,
+    id: 34109,
+    name: "10th / Osage Station",
   },
   {
-    name: "Union Station",
-    id: 33727,
+    id: 34681,
+    name: "13th Avenue Station",
   },
   {
-    name: "Denver Airport",
-    id: 34503,
+    id: 34114,
+    name: "16th St / California Station",
   },
   {
-    name: "40th & Airport - Gateway Park",
+    id: 34115,
+    name: "16th St / Stout Station",
+  },
+  {
+    id: 34116,
+    name: "18th St / California Station",
+  },
+  {
+    id: 34117,
+    name: "18th St / Stout Station",
+  },
+  {
+    id: 34680,
+    name: "2nd Ave / Abilene Station",
+  },
+  {
+    id: 33703,
+    name: "30th / Downing Station",
+  },
+  {
+    id: 34684,
+    name: "38th / Blake Station",
+  },
+  {
     id: 33740,
+    name: "40th / Airport - Gateway Park Station",
+  },
+  {
+    id: 34500,
+    name: "40th / Colorado Station",
+  },
+  {
+    id: 34548,
+    name: "41st / Fox Station",
+  },
+  {
+    id: 35362,
+    name: "48th Ave/Brighton Blvd",
+  },
+  {
+    id: 34506,
+    name: "60th / Sheridan-Arvada Gold Strike",
+  },
+  {
+    id: 34685,
+    name: "61st / Pena Station",
+  },
+  {
+    id: 33704,
+    name: "Alameda Station",
+  },
+  {
+    id: 33705,
+    name: "Arapahoe at Village Center Station",
+  },
+  {
+    id: 34505,
+    name: "Arvada Ridge Station",
+  },
+  {
+    id: 34112,
+    name: "Auraria West Campus Station",
+  },
+  {
+    id: 34678,
+    name: "Aurora Metro Center Station",
+  },
+  {
+    id: 34110,
+    name: "Ball Arena / Elitch Gardens Station",
+  },
+  {
+    id: 33706,
+    name: "Belleview Station",
+  },
+  {
+    id: 34501,
+    name: "Central Park Station",
+  },
+  {
+    id: 34507,
+    name: "Clear Creek / Federal Station",
+  },
+  {
+    id: 33724,
+    name: "Colfax at Auraria Station",
+  },
+  {
+    id: 34682,
+    name: "Colfax Station",
+  },
+  {
+    id: 33708,
+    name: "Colorado Station",
+  },
+  {
+    id: 35363,
+    name: "Commerce City / 72nd Ave",
+  },
+  {
+    id: 33709,
+    name: "County Line Station",
+  },
+  {
+    id: 33710,
+    name: "Dayton Station",
+  },
+  {
+    id: 34232,
+    name: "Decatur-Federal Station",
+  },
+  {
+    id: 34503,
+    name: "Denver Airport Station",
+  },
+  {
+    id: 33711,
+    name: "Dry Creek Station",
+  },
+  {
+    id: 35367,
+    name: "Eastlake / 124th Ave",
+  },
+  {
+    id: 34111,
+    name: "Empower Field at Mile High",
+  },
+  {
+    id: 33712,
+    name: "Englewood Station",
+  },
+  {
+    id: 33713,
+    name: "Evans Station",
+  },
+  {
+    id: 33741,
+    name: "Federal Center Station",
+  },
+  {
+    id: 34683,
+    name: "Fitzsimons Station",
+  },
+  {
+    id: 34679,
+    name: "Florida Station",
+  },
+  {
+    id: 34238,
+    name: "Garrison Station",
+  },
+  {
+    id: 33686,
+    name: "I-25 / Broadway Station",
+  },
+  {
+    id: 34677,
+    name: "Iliff Station",
+  },
+  {
+    id: 34241,
+    name: "Jefferson Government-Golden Station",
+  },
+  {
+    id: 34233,
+    name: "Knox Station",
+  },
+  {
+    id: 34237,
+    name: "Lakewood-Wadsworth Station",
+  },
+  {
+    id: 34236,
+    name: "Lamar Station",
+  },
+  {
+    id: 33714,
+    name: "Lincoln Station",
+  },
+  {
+    id: 33715,
+    name: "Littleton / Downtown Station",
+  },
+  {
+    id: 33716,
+    name: "Littleton / Mineral Station",
+  },
+  {
+    id: 35244,
+    name: "Lone Tree City Center Station",
+  },
+  {
+    id: 33725,
+    name: "Louisiana / Pearl Station",
+  },
+  {
+    id: 33719,
+    name: "Nine Mile Station",
+  },
+  {
+    id: 35366,
+    name: "Northglenn / 112th Ave",
+  },
+  {
+    id: 34239,
+    name: "Oak Station",
+  },
+  {
+    id: 33833,
+    name: "Olde Town Arvada Station",
+  },
+  {
+    id: 33720,
+    name: "Orchard Station",
+  },
+  {
+    id: 35364,
+    name: "Original Thornton / 88th Ave",
+  },
+  {
+    id: 33726,
+    name: "Oxford-City of Sheridan Station",
+  },
+  {
+    id: 34508,
+    name: "Pecos Junction Station",
+  },
+  {
+    id: 34502,
+    name: "Peoria Station",
+  },
+  {
+    id: 34234,
+    name: "Perry Station",
+  },
+  {
+    id: 34240,
+    name: "Red Rocks College Station",
+  },
+  {
+    id: 35245,
+    name: "RidgeGate Parkway Station",
+  },
+  {
+    id: 34235,
+    name: "Sheridan Station",
+  },
+  {
+    id: 35243,
+    name: "Sky Ridge Station",
+  },
+  {
+    id: 33721,
+    name: "Southmoor Station",
+  },
+  {
+    id: 34113,
+    name: "Theatre District / Convention Center",
+  },
+  {
+    id: 35365,
+    name: "Thornton Crossroads / 104th Ave",
+  },
+  {
+    id: 33727,
+    name: "Union Station",
+  },
+  {
+    id: 33722,
+    name: "University of Denver Station",
+  },
+  {
+    id: 34549,
+    name: "Westminster Station",
+  },
+  {
+    id: 34504,
+    name: "Wheat Ridge / Ward Road Station",
+  },
+  {
+    id: 33723,
+    name: "Yale Station",
   },
 ];
 
